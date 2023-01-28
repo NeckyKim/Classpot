4b7ba6013248cd390f9ff6896c5fe3324f96049d
NeckyKim <kyeryoong@gmail.com>
2023-01-29 01:03:00 +0900

    GitHub Actions testing. (230129)

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     return (
         <div className={styles.homeContainer}>
             <div className={styles.homeContainerTop}>
-                간단하게<br />
+                쉽고 간편하게<br />
                 5자리 문자로<br />
                 시험을 진행해보세요.
             </div>
