3ea8cc7a7dd6922c5e6c7c52d3cb58c94b6e3772
NeckyKim <kyeryoong@gmail.com>
2023-04-11 23:32:00 +0900

    PreTestMode page was modified. (230411)

--- src/components/testMode/TestMode.tsx
+++ src/components/testMode/TestMode.tsx
@@ -1199,8 +1199,6 @@ export default function TestMode() {
                     <Error message="유효하지 않은 응시자 입니다." />
             }
 
-            {/* {sampleAlert && <SampleAlertContainer setSampleAlert={setSampleAlert} />} */}
-
             {
                 !testInfo.allowMobile && width < 800
 
